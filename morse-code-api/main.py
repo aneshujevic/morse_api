210211a50ad1e1ebf2e5f120cbe5a423a709e39a
parrot <parrot@localhost.localdomain>
2021-10-23 23:23:04 +0200

    ini

---
+++ morse-code-api/main.py
@@ -0,0 +1,40 @@
+import jwt
+
+from morse_code import MorseCode
+import requests
+
+
+AUTH_API_URL = "http://omega-morse-service.eu-central-1.elasticbeanstalk.com/api/v1/auth"
+RESULT_API_URL = "http://omega-morse-service.eu-central-1.elasticbeanstalk.com/api/v1/result"
+
+
+def sign_in(username, password):
+    data = {'username': username, 'password': password}
+    return requests.request('POST', AUTH_API_URL, json=data)
+
+
+def post_results(first_name, last_name, email, result, github_url):
+    data = {
+        'firstName': first_name,
+        'lastName': last_name,
+        'email': email,
+        'result': result,
+        'githubUrl': github_url
+    }
+    return requests.request('POST', RESULT_API_URL, json=data)
+
+
+if __name__ == '__main__':
+    response = sign_in("omega", "candidate")
+    if response.status_code != requests.codes.ok:
+        print("ERROR SIGNING IN:")
+        print(response.text)
+        exit(-1)
+
+    jwt_token = response.json()["value"]
+    jwt_decoded = jwt.decode(jwt=jwt_token, options={"verify_signature": False})
+    exp_date = jwt_decoded['exp']
+    text_to_encode = f"Vega IT Omega : {exp_date}"
+    encoded_text = MorseCode.encode(text_to_encode)
+    results_resp = post_results("Anes", "Hujevic", "anes1996_h@hotmail.com", encoded_text, "")
+    print(results_resp.json()["message"])
---
+++ morse-code-api/morse_code.py
@@ -0,0 +1,46 @@
+class MorseCode:
+    ENG_TO_MORSE_DICT = {
+        'A': '.-', 'B': '-...', 'C': '-.-.',
+        'D': '-..', 'E': '.', 'F': '..-.',
+        'G': '--.', 'H': '....', 'I': '..',
+        'J': '.---', 'K': '-.-', 'L': '.-..',
+        'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.',
+        'S': '...', 'T': '-', 'U': '..-',
+        'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..', '1': '.----',
+        '2': '..---', '3': '...--', '4': '....-',
+        '5': '.....', '6': '-....', '7': '--...',
+        '8': '---..', '9': '----.', '0': '-----',
+        ',': '--..--', '.': '.-.-.-', '?': '..--..',
+        '-': '-....-', '(': '-.--.', ')': '-.--.-'
+    }
+    MORSE_TO_ENG_DICT = {val: key for key, val in ENG_TO_MORSE_DICT.items()}
+
+    @staticmethod
+    def encode(raw_text):
+        output_text_buffer = []
+        for ch in raw_text:
+            if ch == ' ':
+                output_text_buffer.append('/')
+            else:
+                output_text_buffer.append(MorseCode.ENG_TO_MORSE_DICT[ch])
+
+        return ' '.join(output_text_buffer)
+
+    @staticmethod
+    def decode(encoded_text):
+        output_text_buffer = []
+        enc_character_buffer = []
+
+        for raw_c in encoded_text:
+            if raw_c == '/':
+                output_text_buffer.append(' ')
+            elif raw_c == ' ':
+                if enc_character_buffer:
+                    output_text_buffer.append(MorseCode.MORSE_TO_ENG_DICT[''.join(enc_character_buffer)])
+                    enc_character_buffer.clear()
+            else:
+                enc_character_buffer.append(raw_c)
+
+        return ''.join(output_text_buffer)
